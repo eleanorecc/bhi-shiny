736f922d15c11be3ae8ccaf649f818f79419b051
eleanorecc <eleanorecampbell@ucsb.edu>
2019-11-22 12:32:17 +0100

    gsub instead of str_replace here so dont need library(stringr)

--- R/visualization.R
+++ R/visualization.R
@@ -119,7 +119,7 @@ scores_barplot <- function(scores_csv, basins_or_rgns = "subbasins", goal_code =
     plot_df,
     aes(
       x = pos, y = score_unrounded,
-      text =  sprintf("%s:\n%s", str_replace(Name, ", ", "\n"), Score),
+      text =  sprintf("%s:\n%s", gsub(pattern = ", ", replacement = "\n", Name), Score),
       # Name = Name, Score = Score, Area = Area,
       width = weight, fill = score_unrounded
     )
@@ -167,4 +167,86 @@ scores_barplot <- function(scores_csv, basins_or_rgns = "subbasins", goal_code =
   
   plot_obj <- plotly::ggplotly(plot_obj, tooltip = "text")
   return(invisible(plot_obj))
+}
+
+
+#' @param scores_csv scores dataframe e.g. output of ohicore::CalculateAll
+#' @param basins_or_rgns one of 'subbasins' or 'regions' to indicate which spatial units should be represented
+#' @param rgns vector of bhi region ids by which to filter scores data and for which to create flowerplots
+#' @param goal_code the two or three letter code indicating which goal/subgoal to create the plot for
+#'
+#' @return returns ggplot or html plotly widget if 'make_html' arg is true
+
+future_dims <- function(scores_csv, basins_or_rgns = "subbasins", rgns = c(507, 508, 511), goal_code = "EUT"){
+  
+  if(basins_or_rgns == "subbasins" & !all(rgns %in% 501:517)){
+    stop("the given regions args are not subbasin IDs")
+  }
+
+  ## wrangle for plotting
+  plot_df <- scores_csv %>% 
+    dplyr::mutate(score = ifelse(dimension == "trend", score*100, score)) %>% 
+    dplyr::left_join(
+      dplyr::select(regions_df, region_id, subbasin, area_km2, name = region_name),
+      by = "region_id"
+    ) %>% 
+    rename(id_num = region_id)
+  
+  if(basins_or_rgns == "subbasins"){
+    plot_df <- plot_df %>% 
+      dplyr::select(-id_num) %>% 
+      dplyr::left_join(
+        dplyr::select(subbasins_df, subbasin, id_num = subbasin_id),
+        by = "subbasin"
+      ) %>% 
+      dplyr::filter(id_num %in% rgns, goal %in% goal_codes) %>% 
+      group_by(goal, dimension, subbasin, id_num) %>%
+      summarise(basin_mean = weighted.mean(score, area_km2, na.rm = TRUE))
+  }
+   
+  plot_df$dims_reordered <- factor(
+    plot_df$dimension,
+    levels = c("status", "future", "trend", "pressures", "resilience", "score")
+  )
+  
+  ## color palette
+  dims <- c("present state", "likely future", "trend", "pressures", "resilience", "status")
+  dims_pal <- c("#a0bbd0e8", "#ead19cf0", "#de8b5fe8", "#b13a23db", "#63945ade", "#9483afed")
+  
+  dims_pal <- c("#78a6b0", "#dec887", "#d48c6e", "#b16868", "#80b078", "#a79bbb") # red #a95151 orange #ca7963
+  
+  ## make dimensions bar plot
+  plot_obj <- ggplot(plot_df) + 
+    geom_bar(
+      aes(x = dims_reordered, y = basin_mean, fill = dims_reordered),
+      stat = "identity",  
+      position = "dodge",
+      width = 0.5
+    ) +
+    # coord_cartesian(ylim = c(-70, 100)) +
+    facet_grid(cols = vars(subbasin)) +
+    guides(fill = guide_legend(nrow = 1)) +
+    scale_fill_manual(
+      values = dims_pal,
+      labels = c(
+        "Present Status", 
+        "Likely Future Status",
+        "Trend", 
+        "Pressures", 
+        "Resilience", 
+        "Score"
+      )
+    ) +
+    theme_light() +
+    theme(
+      axis.text.x = element_blank(),
+      panel.border = element_rect(colour = "black", fill = NA),
+      legend.direction = "horizontal",
+      legend.position = c(0.3, 0.06),
+      legend.background = element_rect(size = 0.6, colour = "grey"),
+      axis.ticks.x = element_blank()
+    ) +
+    labs(fill = element_blank(), x = element_blank(), y = element_blank())
+  
+ return(plot_obj) 
 }
