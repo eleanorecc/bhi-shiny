67cddbdb7a8fdb4f0c4bad63c93a2dbd79bc1420
eleanorecc <eleanorecampbell@ucsb.edu>
2019-11-22 11:22:13 +0100

    mapping with sp not sf, rename old mapping script with 'sf'

--- R/mapping_sf.R
+++ R/mapping_sf.R
@@ -1,8 +1,8 @@
-library(sp)
-library(rgdal)
-library(dplyr)
-library(readr)
+library(sf)
 library(leaflet)
+library(readr)
+library(dplyr)
+library(tidyr)
 
 #' create leaflet map
 #'
@@ -28,31 +28,29 @@ leaflet_map <- function(goal_code, mapping_data_sp, basins_or_rgns = "subbasins"
     stop(sprintf("goal must be one of: %s", paste(bhi_goals, collapse = ", ")))
   }
   
-  if(dim != "score" | unique(mapping_data_sp@data$year) != year){
-    mapping_data_sp@data <- dplyr::select(mapping_data_sp@data, -bhi_goals, -year, -dimension)
-    
+  if(dim != "score" | unique(mapping_data_sp$year) != year){
     if(basins_or_rgns == "subbasins"){
-      mapping_data_sp <- make_subbasin_sf(
-        subbasins_shp = mapping_data_sp, 
+      leaflet_plotting_sf <- make_subbasin_sf(
+        subbasins_shp = dplyr::select(mapping_data_sp, -bhi_goals), 
         scores_csv, 
         dim, 
         year
       )
     } else {
-      mapping_data_sp <- make_rgn_sf(
-        bhi_rgns_shp = mapping_data_sp, 
+      leaflet_plotting_sf <- make_rgn_sf(
+        bhi_rgns_shp = dplyr::select(mapping_data_sp, -bhi_goals), 
         scores_csv, 
         dim, 
         year
       )
     }
   } else {
-    if(basins_or_rgns == "subbasins" & any(c("BHI_ID", "region_id") %in% names(mapping_data_sp@data))){
+    leaflet_plotting_sf <- mapping_data_sp
+    if(basins_or_rgns == "subbasins" & any(c("BHI_ID", "region_id") %in% names(mapping_data_sp))){
       stop("BHI regions spatial data given where Subbasin data is expected")
     }
   }
-  mapping_data_sp@data <- dplyr::rename(mapping_data_sp@data, score = goal_code)
-  leaflet_plotting_sf <- mapping_data_sp
+  leaflet_plotting_sf <- dplyr::rename(leaflet_plotting_sf, score = goal_code)
   
   ## theme and map setup ----
   thm <- apply_bhi_theme()
@@ -155,29 +153,16 @@ make_subbasin_sf <- function(subbasins_shp, scores_csv, dim = "score", year = as
     tidyr::spread(key = goal, value = score) %>%
     dplyr::filter(!is.na(Name)) %>%
     dplyr::mutate(dimension = dim, year = year)
- 
-  ## join with spatial information from subbasin shapfile
-  # mapping_data_sp <- subbasins_shp %>%
-  #   dplyr::mutate(Name = as.character(Name)) %>%
-  #   dplyr::mutate(Name = ifelse(
-  #     Name == "Åland Sea",
-  #     "Aland Sea", Name)
-  #   ) %>%
-  #   dplyr::left_join(mapping_data, by = "Name") %>%
-  #   sf::st_transform(crs = 4326)
   
   ## join with spatial information from subbasin shapfile
-  ## spatialdataframes with sp package, rather than sf...
-  subbasins_shp_tab <- subbasins_shp@data %>% 
+  mapping_data_sp <- subbasins_shp %>%
     dplyr::mutate(Name = as.character(Name)) %>%
     dplyr::mutate(Name = ifelse(
       Name == "Åland Sea",
       "Aland Sea", Name)
     ) %>%
-    dplyr::left_join(mapping_data, by = "Name")
-  subbasins_shp@data <- subbasins_shp_tab
-  ## subbasins_shp@proj4string is initially epsg 3035, need epsg 4326 crs for mapping
-  mapping_data_sp <- spTransform(subbasins_shp, CRS("+init=epsg:4326"))
+    dplyr::left_join(mapping_data, by = "Name") %>%
+    sf::st_transform(crs = 4326)
   
   return(mapping_data_sp)
 }
@@ -215,17 +200,9 @@ make_rgn_sf <- function(bhi_rgns_shp, scores_csv, dim = "score", year = assess_y
     dplyr::mutate(dimension = dim, year = year)
   
   ## join with spatial information from subbasin shapfile
-  # mapping_data_sp <- bhi_rgns_shp %>%
-  #   dplyr::mutate(Name = sprintf("%s, %s", Subbasin, rgn_nam)) %>%
-  #   dplyr::left_join(mapping_data, by = "Name")
-  
-  ## join with spatial information from subbasin shapfile
-  ## spatialdataframes with sp package, rather than sf...
-  bhi_rgns_shp_tab <- bhi_rgns_shp@data %>% 
+  mapping_data_sp <- bhi_rgns_shp %>%
     dplyr::mutate(Name = sprintf("%s, %s", Subbasin, rgn_nam)) %>%
     dplyr::left_join(mapping_data, by = "Name")
-  bhi_rgns_shp@data <- bhi_rgns_shp_tab
-  mapping_data_sp <- bhi_rgns_shp
   
   return(mapping_data_sp)
 }
