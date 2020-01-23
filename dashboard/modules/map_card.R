a012faec4392ea1808fb92f4cd493c1861bed0a6
eleanorecc <eleanorecampbell@ucsb.edu>
2020-01-23 15:33:44 +0100

    region highlighting just for main map, remove here

--- dashboard/modules/map_card.R
+++ dashboard/modules/map_card.R
@@ -26,43 +26,19 @@ mapCardUI <- function(id, title_text = NULL, sub_title_text = NULL, box_width = 
   ))
 }
 
-## map card server function ----
 mapCard <- function(input, output, session,
                     goal_code, dimension_selected, spatial_unit_selected, 
-                    year_selected, flower_rgn_selected = NULL,
-                    legend_title, popup_title = NA, popup_add_field = NA, popup_add_field_title = NA){
-
-  
-  rgn_id <- flower_rgn_selected()
+                    year_selected, legend_title, 
+                    popup_title = NA, popup_add_field = NA, popup_add_field_title = NA){
   
   output$plot <- renderLeaflet({
     ## scores data and spatial data
     scores_csv <- full_scores_csv %>%
       filter(dimension == dimension_selected())
-
-    ## create selected-region overlay, based on spatial unit and flowerplot region
-    if(!is.null(rgn_id) && rgn_id %in% c(1:42, 501:517)){
-      
-      if(rgn_id %in% 1:42){
-        rgn_select <- rgns_shp[rgns_shp@data$BHI_ID == rgn_id,]
-        
-      } else if(rgn_id %in% 501:517){
-        subbasins_shp@data <- subbasins_shp@data %>% 
-          dplyr::select(HELCOM_ID) %>% 
-          dplyr::left_join(
-            subbasins_df %>%
-              dplyr::select(BHI_ID = subbasin_id, HELCOM_ID = helcom_id) %>%
-              dplyr::mutate(HELCOM_ID = as.factor(HELCOM_ID)),
-            by = "HELCOM_ID"
-          )
-        
-        rgn_select <- subbasins_shp[subbasins_shp@data$BHI_ID == rgn_id,]
-      }
-    }
     
-   if(spatial_unit_selected() == "subbasins"){
-     mapping_data_sp <- subbasins_shp
-   } else {mapping_data_sp <- rgns_shp}
+    if(spatial_unit_selected() == "subbasins"){
+      mapping_data_sp <- subbasins_shp
+    } else {mapping_data_sp <- rgns_shp}
     
     ## create leaflet map
     result <- leaflet_map(
@@ -75,34 +51,13 @@ mapCard <- function(input, output, session,
       legend_title
     )
     
-    ## create popup text
+    ## create and add popup text
     popup_text <- paste(
       "<h5><strong>", popup_title, "</strong>",
-      result$data_sf[[goal_code]], "</h5>",
+      result$data_sf[["score"]], "</h5>",
       "<h5><strong>", popup_add_field_title, "</strong>",
       result$data_sf[[popup_add_field]], "</h5>", sep = " "
     )
-    
-    ## return leaflet map with popup added
-    if(!is.null(rgn_id) && rgn_id %in% c(1:42, 501:517)){
-      result$map %>%
-        addPolygons(
-          data = rgn_select,
-          stroke = TRUE, weight = 4,
-          opacity = 0.6, fillOpacity = 0, color = "red",
-          smoothFactor = 3) %>%
-        addPolygons(
-          popup = popup_text,
-          fillOpacity = 0,
-          stroke = FALSE
-        )
-    } else {
-      result$map %>%
-        addPolygons(
-          popup = popup_text,
-          fillOpacity = 0,
-          stroke = FALSE
-        )
-    }
+    result$map %>% addPolygons(popup = popup_text, fillOpacity = 0, stroke = FALSE)
   })
 }
