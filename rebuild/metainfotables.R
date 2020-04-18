ce08eb357b1f2ae8e26293a10f5276e9eced29c0
eleanorecc <eleanorecampbell@ucsb.edu>
2020-04-18 21:55:03 +0200

    finalizing data info with fis, mar, np, cw/con data sources

--- rebuild/metainfotables.R
+++ rebuild/metainfotables.R
@@ -1,17 +1,26 @@
 
 
-make_data_table <- function(gh_raw_bhiprep){
+make_data_table <- function(gh_raw_bhiprep, bhi_version){
   
   ## list goal data.rmd files in bhi-prep
   ## read lines in rmds starting at '### 2.1 Datasets' and end at '### 2.2'
   ## identify 4th level headings and bolded lines and linked items
   
+  rmds <- sprintf(c(
+    "CW/contaminants/%s/con_data", 
+    "FIS/%s/fis_np_data",
+    "MAR/v2019/mar_data",
+    "FIS/%s/fis_np_data"
+  ), bhi_version)
+  
   goals <- data.frame(
-    goal = c("CW"),
-    subgoal = c("CON"),
-    datarmdlink = c("CW/contaminants/v2019/con_data"),
+    c("CW", "FP", "FP", "NP"), 
+    c("CON", "FIS", "MAR", NA), 
+    rmds, 
     stringsAsFactors = FALSE
   )
+  colnames(goals) <- c("goal", "subgoal", "rmdlink")
+  
   datalyrs_metainfo <- data.frame(
     goal = character(), 
     subgoal = character(), 
@@ -22,29 +31,36 @@ make_data_table <- function(gh_raw_bhiprep){
     stringsAsFactors = FALSE
   )
 
-  for(g in nrow(goals)){
+  for(g in 1:nrow(goals)){
     
-    txt <- readr::read_lines(sprintf("%sdata/%s.rmd", gh_raw_bhiprep, goals[g, "datarmdlink"]))
+    txt <- readr::read_lines(sprintf("%sdata/%s.rmd", gh_raw_bhiprep, goals[g, "rmdlink"]))
     txt <- txt[grep("^### 2.1 ", txt):grep("^### 2.2 ", txt)]
     
-    headers <- c(grep("^#### ", txt), length(txt))
+    headers <- c(grep("^#### .*[0-9] ", txt), length(txt))
     boldheaders <- grep("^\\*\\*", txt)
     srclnk <- grep("\\[.*\\]\\(.*\\)|http", txt)
     
     lyr <- c()
-    for(h in 2:length(headers)){
-      lyr <- c(lyr, gsub(", $", "", paste(
-        gsub("####.*[0-9] ", "", txt[headers[h-1]]),
-        gsub("\\*\\*", "", txt[boldheaders[boldheaders %in% headers[h-1]:(headers[h])]]),
-        sep = ", "
-      )))
-    }
     src <- c()
     lnk <- c()
-    for(l in srclnk){
-      chk <- gsub(".*\\[", "", stringr::str_split(txt[l], "\\]")[[1]][1])
-      src <- c(src, ifelse(stringr::str_detect(chk, "http"), "Reference", chk))
-      lnk <- c(lnk, gsub("\\)", "", stringr::str_split(stringr::str_extract(txt[l], "http.*"), " ")[[1]][1]))
+    for(h in 2:length(headers)){
+      subheaders <- txt[boldheaders[boldheaders %in% headers[h-1]:(headers[h])]]
+      newlyr <- gsub(", $", "", paste(
+        gsub("#### .*[0-9] ", "", txt[headers[h-1]]),
+        gsub("\\[.*\\]\\(.*\\)", "", gsub("\\*\\*", "", subheaders)),
+        sep = ", "
+      ))
+      lyr <- c(lyr, newlyr)
+      
+      lyrsrc <- srclnk[srclnk %in% headers[h-1]:headers[h]]
+      if(length(lyrsrc) > length(newlyr)){
+        lyrsrc <- lyrsrc[grep("eference point .* \\[.*\\]\\(.*\\)|eference point .* http", txt[lyrsrc])]
+      }
+      for(l in lyrsrc){
+        chk <- gsub(".*\\[", "", stringr::str_split(txt[l], "\\]")[[1]][1])
+        src <- c(src, ifelse(stringr::str_detect(chk, "http"), "Reference", chk))
+        lnk <- c(lnk, gsub("\\)|\\):", "", stringr::str_split(stringr::str_extract(txt[l], "http.*"), " ")[[1]][1]))
+      }
     }
     datalyrs_metainfo <- dplyr::bind_cols(
       goal = rep(goals[g, "goal"], length(lyr)),
@@ -55,6 +71,18 @@ make_data_table <- function(gh_raw_bhiprep){
       sourcelink = lnk
     ) %>% dplyr::bind_rows(datalyrs_metainfo)
   }
+  
+  datalyrs_metainfo <- datalyrs_metainfo %>% 
+    dplyr::mutate(Source = sprintf(
+      "<a href=\"%s\" target = \"_blank\">%s</a>", 
+      sourcelink, source
+    )) %>%
+    mutate(`Goal/Subgoal` = ifelse(is.na(subgoal), goal, subgoal)) %>% 
+    rowwise() %>% 
+    mutate(goal = list(c(goal, subgoal))) %>% 
+    tidyr::unnest(cols = c(goal)) %>% 
+    dplyr::filter(!is.na(goal)) %>% 
+    dplyr::select(goal, `Goal/Subgoal`, Layer = layer, Description = description, Source)
   
   return(datalyrs_metainfo)
 }
